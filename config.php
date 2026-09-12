940e5a09721b4a2cc05c765ccacf2df36f03d074
Abelo-123 <abelo@example.com>
2026-09-13 00:16:57 +0300

    feat: switch upstream SMM provider to godofpanel.com v2 API

--- config.php
+++ config.php
@@ -109,8 +109,9 @@ if ($botToken && strpos($botToken, ':') !== false) {
     }
 }
 
-$gopApiKey = getEnvVar('GODOFPANEL_API_KEY');
-$smmProviderUrl = getEnvVar('SMM_PROVIDER_URL', getEnvVar('PROVIDER_API_URL', 'https://justanotherpanel.com/api/v2'));
+$gopApiKey = getEnvVar('GODOFPANEL_API_KEY', '8951b5e44c5779244a9abccb36f33074');
+$smmProviderUrl = getEnvVar('SMM_PROVIDER_URL', getEnvVar('PROVIDER_API_URL', 'https://godofpanel.com/api/v2'));
+
 $chapaSecretKey = getEnvVar('CHAPA_SECRET_KEY');
 if (!$chapaSecretKey || strpos($chapaSecretKey, 'tEs') !== false || strpos($chapaSecretKey, 'Mg2Kc') !== false) {
     $chapaSecretKey = 'CHASECK-WGUq6JVPIxSmjVSWTebh5UOOcshNscEd';
